f768f4d6354bc993658daf4b2d5c5a515dee2e42
Asmi0816 <asmi0816@740s-pl-114.local>
2017-02-01 12:12:36 -0700

    Did things with the panel to help with the project.

--- src/grid/controller/GridController.java
+++ src/grid/controller/GridController.java
@@ -1,9 +1,20 @@
 package grid.controller;
+import grid.model.ReferenceMaker;
+import grid.view.GridFrame;
 
 public class GridController
 {
+	private GridFrame baseFrame;
+	private ReferenceMaker someMaker;
+	
+	
+	public GridController()
+	{
+		baseFrame = new GridFrame(this);
+	}
 	public void start()
 	{
+	
 		
 	}
 }
--- src/grid/view/GridPanel.java
+++ src/grid/view/GridPanel.java
@@ -5,25 +5,58 @@ import javax.swing.*;
 import java.awt.Dimension;
 import java.awt.event.*;
 import grid.controller.GridController;
+import javax.swing.table.*;
+
 
 public class GridPanel extends JPanel 
 {
-
+	private GridController baseController;
 	private JLabel xLabel;
 	private JLabel yLabel;
 	private JComboBox xPosition;
 	private JComboBox yPosition;
 	private JButton chooseButton;
 	
+	
+	private JScrollPane gridPane;
+	private JTable gridTable;
+	
 	public GridPanel(GridController baseController)
 	{
 		this.xLabel = new JLabel("X-Axis");
 		this.yLabel = new JLabel("Y-Axis");
+		this.chooseButton = new JButton("");
+		
+		setupTable();
+		setupPanel();
+		setupLayout();
+		setupListeners();
+	}
+	
+	private void setupTable()
+	{
+		DefaultTableModel data = new DefaultTableModel(baseController.getGrid(), new String [] {"1","2","3","4","5"})
+		gridTable = new JTable();
+		gridTable.setModel(data);
+		gridPane = new JScrollPane();
+		gridPane.setViewportView(gridTable);
 		
 	}
 	
+	private void setupPanel()
+	{
 	
+	}
+	private void setupLayout()
+	{
 	
+	}
+	private void setupListeners()
+	{
+	
+	}
+	
+
 	
 	
 	
